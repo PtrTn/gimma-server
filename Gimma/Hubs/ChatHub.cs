ce4fbfdcbcdd6453a7d4cdb58c2a7ff0b85ef806
Peter Ton <peterton91@gmail.com>
2021-12-27 18:14:23 +0100

    Implement game starting

--- Gimma/Hubs/ChatHub.cs
+++ Gimma/Hubs/ChatHub.cs
@@ -1,12 +1,28 @@
-﻿using Microsoft.AspNetCore.SignalR;
+﻿using Gimma.Models;
+using Microsoft.AspNetCore.SignalR;
 
 namespace Gimma.Hubs
 {
     public class ChatHub : Hub
     {
-        public async Task SendMessage(string user, string message)
+        private List<Player> _playersInGame = new List<Player>();
+
+        public async Task CreateGame(string userName)
         {
-            await Clients.All.SendAsync("ReceiveMessage", user, message);
+            _playersInGame.Add(new Player(userName, Context.ConnectionId, true));
+            await Clients.Caller.SendAsync("GameCreated", "my-game-id");
+        }
+        
+        public async Task JoinGame(string userName, string gameId)
+        {
+            _playersInGame.Add(new Player(userName, Context.ConnectionId, false));
+            await Clients.Caller.SendAsync("GameJoined");
+        }
+        
+        public async Task StartGame()
+        {
+            Clients.Clients(_playersInGame.Select(o => o._connectionId));
+            await Clients.All.SendAsync("GameStarted");
         }
     }
 }
---
+++ Gimma/Models/Player.cs
@@ -0,0 +1,15 @@
+﻿namespace Gimma.Models;
+
+public class Player
+{
+    public readonly string _userName;
+    public readonly string _connectionId;
+    public readonly bool _isHost;
+
+    public Player(string userName, string connectionId, bool isHost)
+    {
+        _userName = userName;
+        _connectionId = connectionId;
+        _isHost = isHost;
+    }
+}
